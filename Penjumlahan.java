d0c4eda8e3e7051e0f08fa1982150f717bc4d338
Dhikamrd <dhika.marda56@gmail.com>
2024-09-21 20:52:31 +0800

    Add files via upload

---
+++ Penjumlahan.java
@@ -0,0 +1,10 @@
+public class Penjumlahan {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 2;
+        int hasil = angka1 + angka2;
+        System.out.println(hasil);
+    
+    
+    }
+}
